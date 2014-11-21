a0271892ac1275feb6e911fa6bf33eaa299ddc9f
wooorm <tituswormer@gmail.com>
2014-11-21 14:55:35 +0100

    Add retext-inspect to dev-dependecies

--- test.js
+++ test.js
@@ -4,16 +4,18 @@
  * Dependencies.
  */
 
-var stemmer,
+var Retext,
+    stemmer,
+    inspect,
     content,
     visit,
-    Retext,
     assert;
 
-stemmer = require('./');
 Retext = require('retext');
-visit = require('retext-visit');
+stemmer = require('./');
+inspect = require('retext-inspect');
 content = require('retext-content');
+visit = require('retext-visit');
 assert = require('assert');
 
 /**
@@ -23,6 +25,7 @@ assert = require('assert');
 var retext;
 
 retext = new Retext()
+    .use(inspect)
     .use(visit)
     .use(content)
     .use(stemmer);
